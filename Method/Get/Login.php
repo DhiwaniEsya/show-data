127220304d88384d43552eb990da16d1c674b6f5
Dhiwani Maharani Aulia Nur Esa Dhiwani Maharani Aulia Nur Esa <dhiwaniesya28@gmail.com>
2018-10-22 08:58:08 +0700

    ini kedua

---
+++ Method/Get/Login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+	<title>DhiwaniMaharaniAuliaNurEsa</title>
+<body>
+Silahkan Login dengan Username dan Password Anda
+<form action="index.php" method="GET">
+	<table>
+		<tr>
+			<td>Username</td>
+			<td><input type="text" name="username"/></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="password"/></td>
+		</tr>
+		<tr>
+			<td>Level</td>
+			<td><input type="level" name="level"/></td>
+		</tr>
+		<tr>
+			<td>Fullname</td>
+			<td><input type="fullname" name="fullname"/></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Login"/></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
